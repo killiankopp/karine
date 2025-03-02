d3ee670c0ff829065dfb3497dc9609631f9acc8c
Killian KOPP <killiankopp@gmail.com>
2025-03-02 18:19:09 +0100

    v3.1

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ def reduce_colors_with_pillow(image, num_colors):
     reduced_image = reduced_image.convert("RGB")
     return reduced_image
 
-def add_brick_grid(image, line_thickness=1):
+def add_brick_grid(image, line_thickness=1, grid_color="black", scale_factor=8):
     # Augmenter la taille de l'image pour un décalage plus fin
     width, height = image.size
-    enlarged_image = image.resize((width * 4, height * 4), Image.NEAREST)
+    enlarged_image = image.resize((width * scale_factor, height * scale_factor), Image.NEAREST)
 
     # Dessiner la grille
     draw = ImageDraw.Draw(enlarged_image)
-    for y in range(0, height * 4, 4):
-        for x in range(0, width * 4, 4):
-            if (y // 4) % 2 == 0:
-                draw.rectangle([x, y, x + 4, y + 4], outline="black", width=line_thickness)
+    for y in range(0, height * scale_factor, scale_factor):
+        for x in range(0, width * scale_factor, scale_factor):
+            if (y // scale_factor) % 2 == 0:
+                draw.rectangle([x, y, x + scale_factor, y + scale_factor], outline=grid_color, width=line_thickness)
             else:
-                draw.rectangle([x - 2, y, x + 2, y + 4], outline="black", width=line_thickness)
+                draw.rectangle([x - (scale_factor // 2), y, x + (scale_factor // 2), y + scale_factor], outline=grid_color, width=line_thickness)
 
     return enlarged_image
 
@@ -48,6 +48,8 @@ def main():
             new_width = st.number_input("Nouvelle largeur", min_value=1, max_value=width, value=width)
             new_height = st.number_input("Nouvelle hauteur", min_value=1, max_value=height, value=height)
             new_num_colors = st.number_input("Nombre de couleurs souhaité", min_value=1, max_value=num_colors, value=num_colors)
+            line_thickness = st.number_input("Épaisseur des lignes de la grille", min_value=1, value=1)
+            grid_color = st.color_picker("Couleur de la grille", "#000000")
 
             # Bouton de soumission
             submitted = st.form_submit_button("Soumettre")
@@ -62,7 +64,7 @@ def main():
                 st.image(reduced_image, caption="Image avec couleurs réduites", use_column_width=True)
 
                 # Ajouter la grille
-                final_image = add_brick_grid(reduced_image, line_thickness=1)
+                final_image = add_brick_grid(reduced_image, line_thickness=line_thickness, grid_color=grid_color, scale_factor=8)
                 st.image(final_image, caption="Image avec grille en mur de brique", use_column_width=True)
 
 if __name__ == "__main__":
