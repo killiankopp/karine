83f3309d1ec93be854f79c74866b68d5134c5ce3
Killian KOPP <killiankopp@gmail.com>
2025-03-02 18:35:35 +0100

    v3.4

--- app.py
+++ app.py
@@ -2,30 +2,33 @@ import streamlit as st
 from PIL import Image, ImageDraw
 
 def reduce_colors_with_pillow(image, num_colors):
-    # Convertir l'image en mode 'P' avec une palette de couleurs
     reduced_image = image.convert("P", palette=Image.ADAPTIVE, colors=num_colors)
-    # Convertir l'image en mode 'RGB' pour l'affichage
     reduced_image = reduced_image.convert("RGB")
     return reduced_image
 
-def crop_image(image, new_width, new_height):
-    # Calculer les dimensions de recadrage
+def resize_and_crop(image, new_width, new_height):
     width, height = image.size
-    left = (width - new_width) / 2
-    top = (height - new_height) / 2
-    right = (width + new_width) / 2
-    bottom = (height + new_height) / 2
+    aspect_ratio = width / height
 
-    # Recadrer l'image
-    cropped_image = image.crop((left, top, right, bottom))
+    if new_width / aspect_ratio <= new_height:
+        new_size = (new_width, int(new_width / aspect_ratio))
+    else:
+        new_size = (int(new_height * aspect_ratio), new_height)
+
+    resized_image = image.resize(new_size, Image.LANCZOS)
+
+    left = (resized_image.width - new_width) / 2
+    top = (resized_image.height - new_height) / 2
+    right = (resized_image.width + new_width) / 2
+    bottom = (resized_image.height + new_height) / 2
+
+    cropped_image = resized_image.crop((left, top, right, bottom))
     return cropped_image
 
-def add_brick_grid(image, line_thickness=1, grid_color="black", scale_factor=16):
-    # Augmenter la taille de l'image pour un décalage plus fin
+def add_brick_grid(image, line_thickness=1, grid_color="black", scale_factor=64):
     width, height = image.size
     enlarged_image = image.resize((width * scale_factor, height * scale_factor), Image.NEAREST)
 
-    # Dessiner la grille
     draw = ImageDraw.Draw(enlarged_image)
     for y in range(0, height * scale_factor, scale_factor):
         for x in range(0, width * scale_factor, scale_factor):
@@ -37,47 +40,38 @@ def add_brick_grid(image, line_thickness=1, grid_color="black", scale_factor=16)
     return enlarged_image
 
 def main():
-    st.title("Application de traitement d'image")
+    st.title("Enfilage de perles")
 
-    # Téléchargement de l'image
     uploaded_file = st.file_uploader("Téléchargez une image", type=["jpg", "png", "jpeg"])
 
     if uploaded_file is not None:
-        # Lire l'image
         image = Image.open(uploaded_file)
-        st.image(image, caption="Image téléchargée", use_column_width=True)
 
-        # Afficher les dimensions de l'image
         width, height = image.size
-        st.write(f"Largeur: {width}, Hauteur: {height}")
-
-        # Calculer le nombre de couleurs
         num_colors = len(set(image.getdata()))
-        st.write(f"Nombre de couleurs: {num_colors}")
+        st.write(f"{width}px x {height}px - {num_colors} couleurs")
 
-        # Formulaire pour les paramètres
         with st.form("image_processing_form"):
-            new_width = st.number_input("Nouvelle largeur", min_value=1, max_value=width, value=width)
-            new_height = st.number_input("Nouvelle hauteur", min_value=1, max_value=height, value=height)
-            new_num_colors = st.number_input("Nombre de couleurs souhaité", min_value=1, max_value=num_colors, value=num_colors)
-            line_thickness = st.number_input("Épaisseur des lignes de la grille", min_value=1, value=1)
+            new_width = st.number_input("Nb perle en largeur", min_value=1, max_value=width, value=width)
+            new_height = st.number_input("NB perle en  hauteur", min_value=1, max_value=height, value=height)
+            new_num_colors = st.number_input("Nombre de couleurs", min_value=1, max_value=num_colors, value=num_colors)
+            line_thickness = st.number_input("Épaisseur de la grille", min_value=1, value=1)
             grid_color = st.color_picker("Couleur de la grille", "#000000")
 
-            # Bouton de soumission
             submitted = st.form_submit_button("Soumettre")
 
             if submitted:
-                # Recadrer l'image
-                cropped_image = crop_image(image, new_width, new_height)
-                st.image(cropped_image, caption="Image recadrée", use_column_width=True)
+                processed_image = resize_and_crop(image, new_width, new_height)
+                reduced_image = reduce_colors_with_pillow(processed_image, new_num_colors)
 
-                # Réduire le nombre de couleurs
-                reduced_image = reduce_colors_with_pillow(cropped_image, new_num_colors)
-                st.image(reduced_image, caption="Image avec couleurs réduites", use_column_width=True)
+                final_image = add_brick_grid(
+                    image=reduced_image,
+                    line_thickness=line_thickness,
+                    grid_color=grid_color,
+                    scale_factor=64
+                )
 
-                # Ajouter la grille
-                final_image = add_brick_grid(reduced_image, line_thickness=line_thickness, grid_color=grid_color, scale_factor=16)
-                st.image(final_image, caption="Image avec grille en mur de brique", use_column_width=True)
+                st.image(final_image, caption="Perles", use_column_width=True)
 
 if __name__ == "__main__":
     main()
