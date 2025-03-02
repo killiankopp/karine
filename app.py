302bdaf5808cb8658cd25c23d12e9a895238c247
Killian KOPP <killiankopp@gmail.com>
2025-03-02 18:16:00 +0100

    v3.1

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ def reduce_colors_with_pillow(image, num_colors):
     reduced_image = reduced_image.convert("RGB")
     return reduced_image
 
-def add_brick_grid(image):
-    # Doubler la taille de l'image
+def add_brick_grid(image, line_thickness=1):
+    # Augmenter la taille de l'image pour un décalage plus fin
     width, height = image.size
-    enlarged_image = image.resize((width * 2, height * 2), Image.NEAREST)
+    enlarged_image = image.resize((width * 4, height * 4), Image.NEAREST)
 
     # Dessiner la grille
     draw = ImageDraw.Draw(enlarged_image)
-    for y in range(0, height * 2, 2):
-        for x in range(0, width * 2, 2):
-            if (y // 2) % 2 == 0:
-                draw.rectangle([x, y, x + 2, y + 2], outline="black")
+    for y in range(0, height * 4, 4):
+        for x in range(0, width * 4, 4):
+            if (y // 4) % 2 == 0:
+                draw.rectangle([x, y, x + 4, y + 4], outline="black", width=line_thickness)
             else:
-                draw.rectangle([x - 1, y, x + 1, y + 2], outline="black")
+                draw.rectangle([x - 2, y, x + 2, y + 4], outline="black", width=line_thickness)
 
     return enlarged_image
 
@@ -62,7 +62,7 @@ def main():
                 st.image(reduced_image, caption="Image avec couleurs réduites", use_column_width=True)
 
                 # Ajouter la grille
-                final_image = add_brick_grid(reduced_image)
+                final_image = add_brick_grid(reduced_image, line_thickness=1)
                 st.image(final_image, caption="Image avec grille en mur de brique", use_column_width=True)
 
 if __name__ == "__main__":
